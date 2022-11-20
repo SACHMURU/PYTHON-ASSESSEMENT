7f5c2ac26ecf589a2da93968aacf0d673c0f3e51
SACHMURU <118651575+SACHMURU@users.noreply.github.com>
2022-11-20 13:09:07 +0530

    Add files via upload

---
+++ PYTHON ASSESSEMENT.py
@@ -0,0 +1,20 @@
+#QUESTION 2
+
+username=input ("enter the user name")
+password= input ("enter the password")
+if (username=="Sachin" and password=="sachin13"):
+  print ("Welcome Sachin")
+else:
+  print ("wrong credentails")
+#QUESTION 3
+  
+number=int (input ("Values are odd or even"))
+if(number%2)==0:
+    print("It is even number")
+else:
+    print ("It is odd number")
+#QUESTION 4
+
+fruits=["Dragon fruit","kiwi","Muskmelon"]
+for x in fruits :
+    print (x)
